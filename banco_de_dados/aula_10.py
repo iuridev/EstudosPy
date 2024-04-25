9b69773a1dc231827f9fa97c2b75abaca9db45dd
iuri.barreto <iuribarreto.ib@gmail.com>
2024-04-25 00:48:38 -0300

    Classes que representarao as tabelas

--- banco_de_dados/aula_10.py
+++ banco_de_dados/aula_10.py
@@ -1,3 +1,38 @@
-import pymongo
-import sqlalchemy
+# import pymongo
+# import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
+# first pass is to create a Base
+Base = declarative_base()
+
+
+class User(Base):
+    __table_name__ = "user_account"  # create table
+
+    # attributes
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    fullname = Column(String)
+
+    # relation ForeignKey
+    email_user = relationship(
+        "Address", back_populates="user", cascade='all, delete-orphan'
+    )
+
+    def __repr__(self):
+        return f"User( id: {self.id}, name: {self.name}, fullname: {self.fullname} )"
+
+
+class Address(Base):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(30), nullable=False)
+    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
+
+    # relation ForeignKey
+    user = relationship(
+        "User", back_populates="email_user"
+    )
+
+    def __repr__(self):
+        return f"Address( id: {self.id}, Email: {self.email})"
