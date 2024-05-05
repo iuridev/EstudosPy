15d561cd19faef8f3e5a754363860f8f7b053542
iuri.barreto <iuribarreto.ib@gmail.com>
2024-05-05 20:00:46 -0300

    add file university exercice

---
+++ university/university_04.py
@@ -0,0 +1,11 @@
+(True + 2)**2
+
+lista = ["cachorrro", "hamister", ["pato", "galinha","porcp" ], "gato"]
+
+print(lista[3][2])
+
+a = 3
+b = a*2
+a,b = b,a
+
+print(a)
---
+++ university/university_06.py
@@ -0,0 +1,12 @@
+a =0
+for i in range(30):
+    if a%2 == 0:
+        a += 1
+        continue
+    elif a%5 == 0:
+        break
+    else:
+        a += 3
+
+y = 2 + 3 * 5
+print(y)
