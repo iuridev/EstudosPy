36c2c90ac4aacee0bd80de00c790d7b2ce18bdb2
iuri.barreto <iuribarreto.ib@gmail.com>
2024-05-18 23:11:53 -0300

    ADD file #3 sqlite

--- banco_de_dados/BD_relacional/aula_03.py
+++ banco_de_dados/BD_relacional/aula_03.py
@@ -1,3 +1,4 @@
+import pprint
 import sqlite3
 from pathlib import Path
 
@@ -5,6 +6,7 @@ ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / 'bd_studying.db')
 
 cursor = conexao.cursor()
+cursor.row_factory = sqlite3.Row
 
 
 def create_table(conexao, cursor, table):
@@ -32,9 +34,25 @@ def update_user(conexao, cursor, nome, email, idd):
     conexao.commit()
 
 
+def select_user(cursor, idd):
+    cursor.execute('SELECT * FROM clientes WHERE id=?', (idd,))
+    result = cursor.fetchone()
+    pprint.pprint(dict(result))
+
+
+def select_users(cursor):
+    cursor.execute('SELECT * FROM clientes ORDER BY nome')
+    result = cursor.fetchall()
+    for rows in result:
+        pprint.pprint(dict(rows))
+
+
 # update_user(conexao, cursor, 'Iuri', 'iuribarreto@gmail.com', 1)
 
 lis = [('marcos', 'marcos@hotmail.com'), ('yasmim',
                                           'yasmim@yahoo.com'), ('cleber', 'cleber@gmail.com')]
 
 # insert_many_user(conexao, cursor, lis)
+
+select_user(cursor, 1)
+select_users(cursor)
