0f43827516fcf8bd9262c560c791ab6ca41bb3ad
iuri.barreto <iuribarreto.ib@gmail.com>
2024-09-21 11:41:33 -0300

    Retun study

---
+++ list_study/list_comprehension.py
@@ -0,0 +1,6 @@
+listNumbers = [i for i in range(10) if i < 4]
+print(listNumbers)
+
+listNames = ["Iuri", "Daniel", "Bruno", "Maria"]
+listNamesWithU = [name for name in listNames if 'u' in name.lower()]
+print(listNamesWithU)
