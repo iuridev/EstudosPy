d3fa3d69a53663a7d782191132ec329edfa8041a
iuri.barreto <iuribarreto.ib@gmail.com>
2024-09-22 20:51:18 -0300

    fuction lambda

---
+++ lambda/lambda.py
@@ -0,0 +1,14 @@
+'''
+CURIOSIDADE: Lambda é a décima letra do alfabeto grego
+no Python: são funções anônimas
+
+lambda {argumentos}: {expressão}
+
+'''
+
+
+def is_par(x): return x % 2 == 0
+
+
+print(is_par(30))
+print(is_par(27))
