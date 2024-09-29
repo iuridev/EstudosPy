47057f2e14a0223146e87ecee4128586cfc2d613
iuri.barreto <iuribarreto.ib@gmail.com>
2024-09-28 23:24:13 -0300

    Propety study

--- POO/models/06_classe_ex_locadora.py
+++ POO/models/06_classe_ex_locadora.py
@@ -1,22 +1,35 @@
 class Locadora:
-    locadoras = []
+    _locadoras = []
 
     def __init__(self, name) -> None:
-        self.name = name
-        self.ativo = False
-        Locadora.locadoras.append(self)
+        self._name = name
+        self._ativo = False
+        Locadora._locadoras.append(self)
         pass
 
     def __str__(self):
-        return self.name
+        return self._name
 
     def List_Locadoras(self):
-        for loc in self.locadoras:
-            print(vars(loc))
+        print("--- LISTA DE LOCADORAS ---")
+        for loc in self._locadoras:
+            # print(vars(loc))
+            # loc.ativo vai ativar a @property
+            print(f"Biblioteca: {loc._name} | {loc.ativo}")
+
+    def altera_estado(self):
+        self._ativo = not self._ativo
+
+    @property
+    def ativo(self):
+        return "ativado" if self._ativo else "desativado"
 
 
 locadora_1 = Locadora("LocaWeb")
 locacora_2 = Locadora("LocaMAX")
 locadora_3 = Locadora("NetFlix")
+
+Locadora.List_Locadoras(Locadora)
+locacora_2.altera_estado()
 
 Locadora.List_Locadoras(Locadora)
